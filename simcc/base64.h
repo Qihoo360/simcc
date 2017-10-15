c44bacbd9f696ce03daefc995ffe5a96e85b81a9
weizili <zieckey@gmail.com>
2017-10-15 10:14:27 +0800

    Delete two methods: "static string& Encode(string& s)" and "static string& Decode(string& s)"

--- simcc/base64.h
+++ simcc/base64.h
@@ -31,35 +31,10 @@ public:
         return Encode(s.data(), s.size());
     }
 
-    // base 64 encode a string (self-modifying)
-    // 
-    // This function is for C++ only (duh)
-    // 
-    // @param[in,out] s the string to be decoded
-    // @return a reference to the input string
-    static string& Encode(string& s) {
-        string x(Encode(s.data(), s.size()));
-        s.swap(x);
-        return s;
-    }
-
     static string Decode(const char* src, size_t len);
 
     static string Decode(const char* src) {
         return Decode(src, strlen(src));
-    }
-
-    // base 64 decode a string (self-modifying)
-    // On failure, the string is empty.
-    // 
-    // This function is for C++ only
-    // 
-    // @param[in,out] s the string to be decoded
-    // @return a reference to the input string
-    static string& Decode(string& s) {
-        string x(Decode(s.data(), s.size()));
-        s.swap(x);
-        return s;
     }
 
     static string Decode(const string& s) {
