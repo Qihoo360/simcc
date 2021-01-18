03fc1ee606877feed3c6af141d16158aa4752872
wangyitian <wangyitian@360.cn>
2021-01-18 14:53:56 +0800

    to unify the version, by rpm/ and cmake
    rpm version:    libsimcc-1.0.0.174705-1.el6.x86_64  => libsimcc-1.0.1.76-1.el6.x86_64
    cmake version:  safe-simcc-1.0.0.76-1.el6.x86_64    => safe-simcc-1.0.1.76-1.el6.x86_64

--- rpm/build.mk
+++ rpm/build.mk
@@ -5,9 +5,9 @@ SRC			= $(PROJ).tar.gz
 PKG 		= $(wildcard *.spec)
 TEMP_DIR 	= /home/$(shell whoami)/tmp
 ROOT_DIR 	= $(shell PWD=$$(pwd); echo $${PWD%%/$(PROJ)*}/$(PROJ))
-COMMITVERSION  = $(shell expr `git rev-list --all|wc -l` + 174633)
+COMMITVERSION  = $(shell expr `git rev-list --all|wc -l` + 0)
 MAJOR := 1
 MINOR := 0
 FULL := $(MAJOR).$(MINOR)
-RPM_VERSION = $(MAJOR).$(MINOR).0.$(COMMITVERSION)
+RPM_VERSION = $(MAJOR).$(MINOR).1.$(COMMITVERSION)
 
